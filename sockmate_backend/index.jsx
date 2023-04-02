f740abdba534593f1969b0f846a9648764c89990
Lang Cheng (lche262) <79813230+lche262@users.noreply.github.com>
2023-04-02 19:32:50 +1200

    Created the cookie for backend
    Also debugged server keep sending 409 to new users

--- sockmate_backend/index.jsx
+++ sockmate_backend/index.jsx
@@ -1,7 +1,7 @@
 const PORT = 8000
 const express = require('express')
 const {MongoClient} = require('mongodb')
-const { v4: uuidv4 } = require('uuid');
+const {v4: uuidv4} = require('uuid');
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken')
 const uri = 'mongodb+srv://admin:admin@cluster0.vsqizjh.mongodb.net/?retryWrites=true&w=majority'
@@ -11,18 +11,18 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
 
-app.post('/signup',async (req, res) => {
+app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
-    const { email, password }= req.body
+    const {email, password} = req.body
 
     const generatedUserId = uuidv4
-    const hashedPassword = await bcrypt.hash(password,10)
+    const hashedPassword = await bcrypt.hash(password, 10)
 
-    try{
+    try {
         await client.connect()
         const database = client.db('app-data')
         const users = database.collection('users')
@@ -30,45 +30,44 @@ app.post('/signup',async (req, res) => {
 
         const sanitizesEmail = email.toLowerCase()
 
-        const existingUser = users.findOne({sanitizesEmail})
-        console.log(existingUser)
+        const existingUser = await users.findOne({sanitizesEmail})
+        console.log(sanitizesEmail,existingUser)
 
-        if(existingUser){
+        if (existingUser) {
             return res.status(409).send('User already exists. Please login')
         }
 
-        const data= {
+        const data = {
             user_id: generatedUserId,
             email: sanitizesEmail,
             hashed_password: hashedPassword
         }
         const insertedUser = await users.insertOne(data)
 
-        const token = jwt.sign(insertedUser, sanitizesEmail,{
-            expiresIn: 60 * 24 *30,
+        const token = jwt.sign(insertedUser, sanitizesEmail, {
+            expiresIn: 60 * 24 * 30,
         })
 
-        res.status(201).json({ token, userId: generatedUserId, email: sanitizesEmail})
-    }catch (err){
+        res.status(201).json({token, userId: generatedUserId, email: sanitizesEmail})
+    } catch (err) {
         console.log(err)
     }
 })
 
-app.get('/users',async (req, res) => {
+app.get('/users', async (req, res) => {
     const client = new MongoClient(uri)
-    try{
+    try {
         await client.connect()
         const database = client.db('app-data')
         const users = database.collection('users')
 
         const returnedUsers = await users.find().toArray()
         res.send(returnedUsers)
-    }
-    finally {
+    } finally {
         await client.close()
     }
 
 })
 
 
-app.listen(PORT , () => console.log('Server running on PORT'+ PORT))
+app.listen(PORT, () => console.log('Server running on PORT' + PORT))
--- sockmate_frontend/src/components/AuthModal.jsx
+++ sockmate_frontend/src/components/AuthModal.jsx
@@ -1,12 +1,14 @@
 import {useState} from "react";
 import axios from 'axios';
 import {useNavigate} from "react-router-dom";
+import { useCookies } from 'react-cookie';
 
 const AuthModal = ({setShowModal, isSignUp}) =>{
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
     const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
+    const [cookie,setCookie, removeCookie] = useCookies('user')
 
     let navigate = useNavigate()
 
@@ -23,6 +25,10 @@ const AuthModal = ({setShowModal, isSignUp}) =>{
             }
 
             const response = await axios.post('http://localhost:8000/signup', {email, password})
+
+            setCookie('Email',response.data.email)
+            setCookie('userId', response.data.userId)
+            setCookie('AuthToken', response.data.token)
 
             const success = response.status == 201
             if (success) navigate('/onboarding')
