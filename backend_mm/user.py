ac3f9414d208f2ace86e37a3b8c6a876a9e2b0d8
HZ991 <79825123+HZ991@users.noreply.github.com>
2023-07-11 14:59:28 +1200

    added signup request (test POST api)

--- backend_mm/user.py
+++ backend_mm/user.py
@@ -1,6 +1,21 @@
-from flasgger import swag_from
+from flasgger import Swagger, swag_from
+from flask import Flask, request
 from flask.views import MethodView
+import mysql.connector
+import match
 
+app = Flask(__name__)
+swagger = Swagger(app)
+
+cnx = mysql.connector.connect(
+    user='root',  # MySQL username CHANEG TO 'admin1' FOR DEPLOYMENT 
+    password='mixnmatchmysql',  # MySQL password
+    host='localhost',  # IP address or hostname
+    database='mixnmatch'  # MySQL database
+)
+
+# Create a cursor object to execute SQL queries
+cursor = cnx.cursor()
 
 class UserAPI(MethodView):
 
@@ -12,12 +27,31 @@ class UserAPI(MethodView):
         else:
             # expose a single user
             pass
-
+    
+    @app.route('/signup', methods=['POST'])
     @swag_from('openapi.yml')
-    def post(self):
+    def post():
         # create a new user
-        pass
+        email = request.form.get('email')
+        username = request.form.get('username')
+        password = request.form.get('password')
 
+        # check if email has been registered
+        email_query = "SELECT COUNT(*) FROM user WHERE email = '" + email + "'"
+        cursor.execute(email_query)
+        result = cursor.fetchone()
+        if (result[0] != 0) :
+            return 'This email is already registered, try another one!'
+
+         # Inserting the new user into the database
+        insert_query = "INSERT INTO user (email, username, password) VALUES (%s, %s, %s)"
+        user_data = (email, username, password)
+        cursor.execute(insert_query, user_data)
+        cnx.commit()
+
+        return 'User registered successfully!'
+
+    @app.route('/update-user/<int:user_id>', methods=['PUT'])
     @swag_from('openapi.yml')
     def put(self, user_id):
         # update a single user
@@ -27,3 +61,7 @@ class UserAPI(MethodView):
     def delete(self, user_id):
         # delete a single user
         pass
+
+
+if __name__ == '__main__':
+    app.run()
