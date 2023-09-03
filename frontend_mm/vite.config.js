7035135fe4d927d091b9cab1c7f51090a13d0dea
Lang Cheng (lche262) <79813230+lche262@users.noreply.github.com>
2023-09-04 00:34:45 +1200

    change vite.config

--- frontend_mm/vite.config.js
+++ frontend_mm/vite.config.js
@@ -1,15 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'src/main.jsx', // Adjust this to your main entry file
-      },
-    },
-  },
-  optimizeDeps: {
-    include: ['vue'], // Example: Specify dependencies to optimize
-  },
+  plugins: [react()],
 })
