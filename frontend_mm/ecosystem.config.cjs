057e88b4f6249f2dd9b14a09e8064bba1f464083
Lang Cheng (lche262) <79813230+lche262@users.noreply.github.com>
2023-09-03 23:28:26 +1200

    change ecosystm.config.cjs to work with  ES module syntax

--- frontend_mm/ecosystem.config.cjs
+++ frontend_mm/ecosystem.config.cjs
@@ -1,16 +1,16 @@
-module.exports = {
+export default {
     apps: [
         {
-            name: "mixandmatch",
-            script: "node_modules/.bin/vite",
-            args: "start:frontend",
+            name: 'mixandmatch',
+            script: 'node_modules/.bin/vite',
+            args: 'start:frontend',
             autorestart: true,
             watch: false,
             env: {
-                NODE_ENV: "development",
+                NODE_ENV: 'development',
             },
             env_production: {
-                NODE_ENV: "production",
+                NODE_ENV: 'production',
             },
         },
     ],
