a36a5e6973e184b954f8c3eb118572805b28e810
Lang Cheng (lche262) <79813230+lche262@users.noreply.github.com>
2023-09-04 08:07:19 +1200

    change the BrowserRouter to HashRouter

--- frontend_mm/src/App.jsx
+++ frontend_mm/src/App.jsx
@@ -5,19 +5,19 @@ import Home from './pages/Home.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import OnBoarding from './pages/OnBoarding.jsx'
 import Chatting from "./pages/Chatting.jsx";
-import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import {HashRouter, Routes, Route } from 'react-router-dom'
 
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={ <Home/>}/>
         <Route path="/dashboard" element={ <Dashboard/>}/>
         <Route path="/onboarding" element={ <OnBoarding/>}/>
           <Route path="/chatting" element={ <Chatting/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
