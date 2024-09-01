9a99f2d311c93903439a641320f97c0888d7e49a
codecreator127 <codecreator127@gmail.com>
2024-09-01 20:29:21 +1200

    feat: commented out category part in the chat.py

--- backend_mm/chat.py
+++ backend_mm/chat.py
@@ -75,74 +75,74 @@ async def chat_server(websocket, path):
             print(connected_clients)
 
             # Check if the recipient is a bot
-            with cnx.cursor() as cursor:
-                query = "SELECT * FROM user_category WHERE user_id = %s"
-                cursor.execute(query, (receiver_id,))
-                result = cursor.fetchone()
-                print(result)
-                if result and result[1] == 'BOT':
-                    user_id = str(sender_id)  # Convert sender_id to a string
+            # with cnx.cursor() as cursor:
+            #     query = "SELECT * FROM user_category WHERE user_id = %s"
+            #     cursor.execute(query, (receiver_id,))
+            #     result = cursor.fetchone()
+            #     print(result)
+            #     if result and result[1] == 'BOT':
+            #         user_id = str(sender_id)  # Convert sender_id to a string
 
-                    query = "SELECT * FROM user WHERE id = %s"
-                    cursor.execute(query, (user_id,))
-                    user_attributes = cursor.fetchone()
+            #         query = "SELECT * FROM user WHERE id = %s"
+            #         cursor.execute(query, (user_id,))
+            #         user_attributes = cursor.fetchone()
 
-                    chat_history_query = ("SELECT * FROM message WHERE (sender_id = %s AND receiver_id = %s) OR ("
-                                          "receiver_id = %s AND sender_id = %s)")
-                    cursor.execute(chat_history_query, (sender_id, receiver_id, sender_id, receiver_id))
-                    chat_history = cursor.fetchall()
-                    print(chat_history)
+            #         chat_history_query = ("SELECT * FROM message WHERE (sender_id = %s AND receiver_id = %s) OR ("
+            #                               "receiver_id = %s AND sender_id = %s)")
+            #         cursor.execute(chat_history_query, (sender_id, receiver_id, sender_id, receiver_id))
+            #         chat_history = cursor.fetchall()
+            #         print(chat_history)
 
-                    if user_attributes:
-                        user_name = user_attributes[2]
-                        user_age = user_attributes[4]
-                        user_gender = user_attributes[5]
-                        user_education = user_attributes[7]
+                #     if user_attributes:
+                #         user_name = user_attributes[2]
+                #         user_age = user_attributes[4]
+                #         user_gender = user_attributes[5]
+                #         user_education = user_attributes[7]
 
-                        # Generate a response using OpenAI GPT-3.5-turbo
-                        response = openai.ChatCompletion.create(
-                            model="gpt-3.5-turbo",
-                            messages=[
-                                {"role": "system", "content": f"IMPORTANT MESSAGE: Pretend to have normal "
-                                                              f"conversation to me like others dating app user. "
-                                                              f"Here are some roles that you need to follow: "
-                                                              f"You are a user in an dating app called mix and match "
-                                                              f"provide continuous conversation to the user base on the "
-                                                              f"chat history you and the user had {chat_history}."
+                #         # Generate a response using OpenAI GPT-3.5-turbo
+                #         response = openai.ChatCompletion.create(
+                #             model="gpt-3.5-turbo",
+                #             messages=[
+                #                 {"role": "system", "content": f"IMPORTANT MESSAGE: Pretend to have normal "
+                #                                               f"conversation to me like others dating app user. "
+                #                                               f"Here are some roles that you need to follow: "
+                #                                               f"You are a user in an dating app called mix and match "
+                #                                               f"provide continuous conversation to the user base on the "
+                #                                               f"chat history you and the user had {chat_history}."
                                                               
-                                                              f"Here is your basic information as a dating app user:"
-                                                              f"for you to role play: "
-                                                              f"The name of the person you are going to role play is {user_name}"
-                                                              f"The age of the person you are going to role play is {user_age},"
-                                                              f"The gender of the person you are going to role play is {user_gender} and "
-                                                              f"The education of the person you are going to role play is{user_education}."},
+                #                                               f"Here is your basic information as a dating app user:"
+                #                                               f"for you to role play: "
+                #                                               f"The name of the person you are going to role play is {user_name}"
+                #                                               f"The age of the person you are going to role play is {user_age},"
+                #                                               f"The gender of the person you are going to role play is {user_gender} and "
+                #                                               f"The education of the person you are going to role play is{user_education}."},
 
-                                {"role": "user", "content": chat_message}
-                            ]
-                        )
+                #                 {"role": "user", "content": chat_message}
+                #             ]
+                #         )
 
-                        # Extract the generated message from the response
-                        bot_response = response.choices[0].message["content"].strip()
-                        print(bot_response)
+                #         # Extract the generated message from the response
+                #         bot_response = response.choices[0].message["content"].strip()
+                #         print(bot_response)
 
-                        # Create a message to send
-                        bot_message = {
-                            "sender_id": receiver_id,
-                            "receiver_id": sender_id,
-                            "message": bot_response,
-                            "status": "unread"
-                        }
+                #         # Create a message to send
+                #         bot_message = {
+                #             "sender_id": receiver_id,
+                #             "receiver_id": sender_id,
+                #             "message": bot_response,
+                #             "status": "unread"
+                #         }
 
-                        insert_message(bot_message["sender_id"], bot_message["receiver_id"], bot_message["message"],
-                                       bot_message["status"], cnx)
+                #         insert_message(bot_message["sender_id"], bot_message["receiver_id"], bot_message["message"],
+                #                        bot_message["status"], cnx)
 
-                        # Send the message as JSON
-                        await websocket.send(json.dumps(bot_message))
-                else:
+                #         # Send the message as JSON
+                #         await websocket.send(json.dumps(bot_message))
+                # else:
                     # Send the message to the other user (recipient)
-                    recipient_ws = connected_clients.get(str(receiver_id))
-                    if recipient_ws:
-                        await recipient_ws.send(message)
+            recipient_ws = connected_clients.get(str(receiver_id))
+            if recipient_ws:
+                await recipient_ws.send(message)
 
     except websockets.exceptions.ConnectionClosedError:
         pass  # Handle disconnection
